14edfc27b20413b5fdf988773d460927f3cb6dc6
Aydin <aydindavut14@gmail.com>
2020-09-13 02:27:48 +0300

    Updates

---
+++ precache-manifest.2b89b034477bfdc848da4c50fddd1f2a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33a382bea87a3c75683a7352392c8ccd",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "c20d2fac7ab1adc4d29f",
+    "url": "/my-portfolio/static/css/2.abc88e16.chunk.css"
+  },
+  {
+    "revision": "6dff2749a28bc84c182b",
+    "url": "/my-portfolio/static/css/main.8efb8bcb.chunk.css"
+  },
+  {
+    "revision": "c20d2fac7ab1adc4d29f",
+    "url": "/my-portfolio/static/js/2.26d9801b.chunk.js"
+  },
+  {
+    "revision": "297829219adad9f6a5d583ce8f33a8b9",
+    "url": "/my-portfolio/static/js/2.26d9801b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dff2749a28bc84c182b",
+    "url": "/my-portfolio/static/js/main.d59210b2.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  },
+  {
+    "revision": "c6d37b26297461a78d10f169dd5cf46d",
+    "url": "/my-portfolio/static/media/Avenir-Roman-12.c6d37b26.ttf"
+  },
+  {
+    "revision": "8ea32e99070762d4e4803000c16f91d9",
+    "url": "/my-portfolio/static/media/Valencia.8ea32e99.ttf"
+  },
+  {
+    "revision": "effd9868505effa943808f72aea7ba2f",
+    "url": "/my-portfolio/static/media/letter-a.effd9868.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/my-portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/my-portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/my-portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/my-portfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
